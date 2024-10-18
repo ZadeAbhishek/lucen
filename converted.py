eceebe48c9c0e898f3c19403bf6e9c7e1a8ca772
Abhishek Zade <abhishekzade@Abhisheks-MacBook-Pro.local>
2024-10-18 12:54:35 +0100

    Updated cranqrel

--- converted.py
+++ converted.py
@@ -8,5 +8,8 @@ with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
             query_id = parts[0]
             doc_id = parts[1]
             relevance = parts[2]
+            # Replace -1 with 5 in relevance
+            if relevance == '-1':
+                relevance = '5'
             # Write in TREC qrels format: query_id iteration doc_id relevance
             outfile.write(f"{query_id} 0 {doc_id} {relevance}\n")
